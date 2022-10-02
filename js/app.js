14c97ec42d4d5892ffe886c6adf336e0aae1d368
Brian Jensen <31598338+brian-jensen@users.noreply.github.com>
2022-10-02 07:55:03 -0700

    Simplify RegEx for Safari support

--- js/app.js
+++ js/app.js
@@ -2,14 +2,13 @@ const submit = document.querySelector('button[type="submit"]');
 const email = document.querySelector('input');
 const form = document.querySelector('form');
 
-const rfc5322 = /^((?:[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){2,})$/gm
+const rfc5322 = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
   if (form.lastElementChild !== submit) {
       form.lastElementChild.remove();
   }
-
   if (email.value === '') {
     email.classList.add('error');
     form.insertAdjacentHTML('beforeend', `<span>Oops! Please add your email</span>`)
